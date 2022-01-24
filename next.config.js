0124cbf0b7aa7ddb52f2840a97fb13ee49f51810
kanpizza <kanpizza1150@gmail.com>
2022-01-24 10:01:14 +0700

    added image domain config

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
+  images: {
+    domains: ['https://k39.netlify.app/'],
+  },
 }
 
 module.exports = nextConfig
